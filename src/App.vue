efc4377acea84b3e499596a15c4cc63387e76243
saintlyly <lucktao8@163.com>
2019-09-29 18:10:54 +0800

    newtabbar

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,20 @@
 <template>
   <div id="app">
-    <tab-bar>
-      <tab-bar-item link="/home">
-       <img slot="icon" src="@/assets/img/tabbar/home.svg" alt="">
-       <img slot="active-icon" src="@/assets/img/tabbar/home_active.svg" alt="">
-      <div slot="text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item link="/category">
-       <img slot="icon" src="@/assets/img/tabbar/category.svg" alt="">
-       <img slot="active-icon" src="@/assets/img/tabbar/category_active.svg" alt="">
-      <div slot="text">分类</div>
-      </tab-bar-item>
-      <tab-bar-item link="/cart">
-       <img slot="icon" src="@/assets/img/tabbar/cart.svg" alt="">
-       <img slot="active-icon" src="@/assets/img/tabbar/cart_active.svg" alt="">
-      <div slot="text">购物车</div>
-      </tab-bar-item>
-      <tab-bar-item link="/profile">
-       <img slot="icon" src="@/assets/img/tabbar/profile.svg" alt="">
-       <img slot="active-icon" src="@/assets/img/tabbar/profile_active.svg" alt="">
-      <div slot="text">我的</div>
-      </tab-bar-item>
-      </tab-bar>
-    <router-view/>
+    <foot-guide></foot-guide>
+    <router-view />
   </div>
 </template>
 
 <script>
-import TabBar from "@/components/tabbar/TabBar.vue"
-import TabBarItem from "@/components/tabbar/TabBarItem.vue"
+import footGuide from "@/components/footer/footGuide.vue";
 
-export default{
-  name:'app',
-  components:{
-    TabBar,
-    TabBarItem,
+export default {
+  name: "app",
+  components: {
+    footGuide
   }
-
-}
+};
 </script>
 
 <style lang="scss">
-
 </style>
--- src/components/tabbar/TabBarItem.vue
+++ src/components/tabbar/TabBarItem.vue
@@ -1,32 +1,37 @@
 <template>
   <div class="tab-bar-item" @click="itemClick">
-    <div class="item-icon" v-show="!isActive"><slot name="icon"></slot></div>
-    <div class="item-active-icon" v-show="isActive"><slot name="active-icon"></slot></div>
-    <div class="item-text" :style="activeStyle"><slot name="text"></slot></div>
+    <div class="item-icon" v-show="!isActive">
+      <slot name="icon"></slot>
+    </div>
+    <div class="item-active-icon" v-show="isActive">
+      <slot name="active-icon"></slot>
+    </div>
+    <div class="item-text" :style="activeStyle">
+      <slot name="text"></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "tabbaritem",
-  props:{
-      link:{
-          type:String,
-          required:true,
-      }
+  props: {
+    link: {
+      type: String,
+      required: true
+    }
   },
-  computed:{
-      isActive(){
-          return this.$route.path.indexOf(this.link) !== -1
-     
-      },
-      activeStyle(){
-          return this.isActive?{'color':'red'}:{}
-      }
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.link) !== -1;
+    },
+    activeStyle() {
+      return this.isActive ? { color: "red" } : {};
+    }
   },
   methods: {
     itemClick() {
-        this.$router.replace(this.link)
+      this.$router.replace(this.link);
     }
   }
 };
